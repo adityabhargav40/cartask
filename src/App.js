14d7528adeda1d978cf16305ba24cbd4bb70fd28
adityabhargav40 <adityabhargav40@gmail.com>
2023-06-13 15:22:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Clock from './clock';
+import CarList from './car';
+
+function App() {
+  return (
+    <div>
+      <Clock />
+      <CarList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/clock.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (date) => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  return (
+    <div>
+      <h1>current time: {formatTime(time)}</h1>
+     
+    </div>
+  );
+}
+
+
+export default Clock;
+
